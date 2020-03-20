07a88f4f1cd4190791e3778d9112bbff7ad1b1d9
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-03-19 20:24:50 -0400

    navbar tweaking

--- overview/layouts/default.vue
+++ overview/layouts/default.vue
@@ -8,13 +8,46 @@
           <v-col class="navbarColumn">
             <a class="logo" href="#"><img class="logoImg" src="../assets/images/logo/logo.png" alt=""></a>
             
-            <ul class="navbarList">
+            <v-btn color="#BF3F4A" class="white--text">
+              Download App
+            </v-btn>
+
+
+            <!-- @Isaiah this is where menu code begins.  The commented out <ul> a bit further down is what I'm attemtping to replace with this dropdown menu so at some point we need to get the links to other pages in the menu items-->
+            <v-menu offset-y>
+              <v-btn text slot="activator" color="gray">
+                <v-icon left>mdi-chevron-down</v-icon>
+                <span>Menu</span>
+              </v-btn>
+              <v-list>
+                <v-list-item>
+                  hey
+                </v-list-item>
+
+
+
+                <!-- <v-list-item
+                  v-for="(item, i) in items"
+                  :key="i"
+                  @click=""
+                >
+                  <v-list-item-title>
+                    hey
+                  </v-list-item-title>
+
+                </v-list-item> -->
+              </v-list>
+
+            </v-menu>
+
+
+            <!-- <ul class="navbarList">
               <li><a target="_blank" class="link heatmapLink text-no-wrap" href="https://www.covid19risk.com/heatmap/">Heatmap</a></li>
               <li><a target="_blank" class="link text-no-wrap" href="https://www.covid19risk.com/collaborate.html">Get Involved</a></li>
               <li><a target="_blank" class="link text-no-wrap" href="https://www.covid19risk.com/collaborate.html">Donate</a></li>
               <li><a target="_blank" class="link text-no-wrap" href="https://www.covid19risk.com/about.html">About</a></li>
               <li class="download text-no-wrap">Download App</li>
-            </ul>
+            </ul> -->
           </v-col>
         </v-row>
       </v-container>
@@ -26,10 +59,46 @@
       </v-container>
     </v-content>
 
+     <v-footer color="#A2C5BF" :elevation="6">
+      <v-card
+        flat
+        tile
+        class="lighten-1 white--text text-center"
+        color="#A2C5BF"
+      >
+      
+     
+
+      <div class="footerList">
+       <a target="_blank"  class="text-no-wrap" href="https://www.covid19risk.com/heatmap/">Heatmap</a>
+        <a target="_blank" class="text-no-wrap" href="https://www.covid19risk.com/collaborate.html">Get Involved</a>
+        <a target="_blank" class="text-no-wrap" href="https://www.covid19risk.com/collaborate.html">Donate</a>
+        <a target="_blank" class="text-no-wrap" href="https://www.covid19risk.com/about.html">About</a>
+      </div>
+  
+        <v-divider></v-divider>
+  
+        <v-card-text class="gray--text">
+          All rights reserved - COVID Watch
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
+
 export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' }
+      ]
+    }
+  }
 }
 </script>

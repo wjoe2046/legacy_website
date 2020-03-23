eafba4e09f3cd6a655beed9bb281cef059696707
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-03-22 22:17:59 -0400

    add Collaborate.vue file

---
+++ overview/pages/Collaborate.vue
@@ -0,0 +1,77 @@
+<template>
+    <section class="text-content">
+    <div class="container">
+    <div class="blog-hero"></div>
+
+    <h1 style="color:#BF3F4A">Join Our Team</h1>
+
+    <p>We&#39;ve grown from a pair of researchers from Stanford University and the University of Waterloo into an interdisciplinary team
+    of researchers, software engineers, public health and security experts. But we still need more help. We are looking for
+    more volunteers to join us and help us build an app that uses crowdsourced data to inform individuals of their
+    risk and slow the spread of COVID-19.</p>
+
+    <h3>Positions Wanted</h3>
+
+    <p>(1) Volunteer Coordinator - coordinate the efforts, schedules, tasks among 10-30 remote volunteers in various roles</p>
+
+    <p>(2) Project Manager - coordinate the tasks and workflow for group of software engineers</p>
+
+    <p>(3) Personal Assistant - assist Executive Director with various admin tasks</p>
+
+    <p>(4) Volunteer Recruiter - get the word out, bring more people onto our team, then get them situated and productive quickly</p>
+
+    <h3>Support Roles</h3>
+
+    <p>Even if you don&#39;t have a skillset directly related to the project&#39;s design or development, there are still ways to help. We&#39;re a rapidly expanding group, and we&#39;re also interested if you have experience in:</p>
+
+    <p>(1) Fundraising and grant applications for not-for-profit organisations</p>
+
+    <p>(2) PR and Outreach</p>
+
+    <p>(3) Blogging, particularly with a scientific background</p>
+
+    <p>(4) Onboarding and orientation into organisations</p>
+
+    <h3>Research Roles</h3>
+
+    <p>We&#39;re currently looking for people with one (or more!) of the following skills or backgrounds:</p>
+
+    <p>(1) General mobile app UI design (iOS and/or Android)</p>
+
+    <p>(2) Advanced web development skills, particularly in interactive visualisations</p>
+
+    <p>(3) Mobile app development skills</p>
+
+    <p>(4) Software project management</p>
+
+    <p>(6) Legal expertise around the handling of personal health information</p>
+
+    <p>(7) Experience in the fields of data re-identification or privacy research and implementation.</p>
+
+    <p>(7) Crypto / security experience</p>
+
+    <p>(8) Experience with bluetooth on Android and iOS </p>
+
+
+    <h3>Applying</h3>
+
+    <p>If you know of anyone who could fill these roles, get them to email us at <b><a class="blue" href="mailto: contact@covid-watch.org">contact@covid-watch.org </a></b>.</p>
+
+    <p>We recommend you read the <b><a class="blue" href="https://forum.effectivealtruism.org/posts/8chk6DHZXctGHtNoz/covid-19-risk-assessment-app-idea-for-vetting-and-discussion">forum post </a></b> to have the context to understand what you&#39;d be working on and the motivations behind the project. The post was optimistic about what could be accomplished in a short time period, and we are no longer sure about providing scores and recommendations, but we think we may be able to build the heat map referenced in the post with help from other researchers.</p>
+
+    <p>If you&#39;re interested in collaborating and have one of the skillsets listed above, please contact us at <b><a class="blue" href="mailto: contact@covid-watch.org">contact@covid-watch.org </a></b> with the role you would like to fill and a brief (~300 words) introduction with your skills and links to previous work.</p>
+
+    <p>We're also interested if you have information that you think could be relevant to this project. In particular, we are interested in any other teams that are currently, or were previously, engaged in similar efforts.</p>
+
+    <h2>Other ways to help</h2>
+
+    <h3>Donations</h3>
+
+    <p>We are currently exploring funding options. We do not currently have a way to donate directly, but if you have information or inquiries related to funding, we'd love to hear from you at the email above.</p>
+
+    <h3>Awareness</h3>
+
+    <p>Even if you don&#39;t have the ability to directly support us right now, consider sharing our project over social media or by word of mouth. To keep up to date with our progress, or to see if a role that suits you becomes available, you can sign up to our newsletter via the form on our <b><a class="blue" href="index.html">homepage </a></b>.</p>
+    </div>
+    </section>
+</template>

1060bb5c54c63c4a2ca64880ab566eae165d9b24
RhysFenwick <rhys.a.fenwick@gmail.com>
2020-03-24 23:16:40 +1100

    Added placeholder donations text.

--- overview/pages/Donate.vue
+++ overview/pages/Donate.vue
@@ -2,7 +2,11 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="12" lg="8">
-        <h3>Donations</h3>
+        <h2>Donations</h2>
+        <p>
+          We're still in the process of setting up a donations system.
+           Check back here in a few days!
+        </p>
       </v-col>
     </v-row>
   </v-container>

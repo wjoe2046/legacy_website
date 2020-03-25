fe8483a086259a859d8a4ef32dc1913441f8bfc8
RhysFenwick <61520663+RhysFenwick@users.noreply.github.com>
2020-03-25 16:10:36 +1100

    Fixed whatever commenting stuff was going on

--- overview/pages/insight1.vue
+++ overview/pages/insight1.vue
@@ -1,4 +1,4 @@
-<!--<template>
+<template>
   <v-container>
     <v-row justify="center">
       <v-col cols="12" lg="8">
@@ -106,15 +106,14 @@
                   </div>
                   <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
                   <!--End mc_embed_signup-->
-
-//                 </div>
-//               </div>
-//             </section>
-//           </v-col>
-//         </v-row>
-//       </v-col>
-//     </v-row>
-//   </v-container>
-// </template>-->
+                </div>
+              </div>
+            </section>
+          </v-col>
+         </v-row>
+        </v-col>
+       </v-row>
+      </v-container>
+</template>
     
   

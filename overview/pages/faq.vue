8d2d5bb38846677af68e15bf5843b206bc156d9e
oozio <chewyslonglonglongemail@gmail.com>
2020-03-27 23:54:31 -0700

    faq skeleton

---
+++ overview/pages/faq.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" lg="8">
+        <section class="text-content">
+          <div class="container">
+            <div class="blog-hero"></div>
+
+            <h1 style="color:#BF3F4A">Frequently Asked Questions</h1>
+
+            <h3>Question?</h3>
+            <p>
+            Answer!
+            </p>
+
+            <h3>Who are you?</h3>
+
+            <p>
+             We’re a non-profit group of researchers, programmers security experts and public health professionals. Starting from a forum post on February 19, our team, led by researchers from Stanford University and the University of Waterloo, has grown to a group of around 10-15 core contributors, as well as many more part-time volunteers and advisors. Although we are aiming to develop an app for initial use in the USA, the team is based around the world.
+            </p>
+            <p>
+             Check our "About Us" page for more information.
+            </p>
+
+            <h3>Why are you doing this?</h3>
+            <p>
+            </p>
+
+            <h3>Aren’t there a lot of people working on this already?</h3>
+            <p>
+            </p>
+
+            <h3>Why is this necessary?</h3>
+            <p>
+            </p>
+
+            <h3>What is contact tracing?</h3>
+            <p>
+            </p>
+
+            <h3>If COVID-19 is going to become widespread anyway, why bother delaying the inevitable?</h3>
+            <p>
+            </p>
+
+            <h3>There is already a lockdown in my country / state and we can’t meet other people, so what is the point?</h3>
+            <p>
+            </p>
+
+            <h3>Is this legal given health data privacy laws?</h3>
+            <p>
+            </p>
+
+            <h3>Will this app ever get enough traction to work?</h3>
+            <p>
+            </p>
+
+            <h3>Is this just another app that will record my symptoms?</h3>
+            <p>
+            </p>
+
+            <h3>Is this similar to what South Korea and Singapore are doing?</h3>
+            <p>
+            </p>
+
+            <h3>Is this the same as what Hong Kong has introduced?</h3>
+            <p>
+            </p>
+
+            <h3>What is the legal status of your organization?</h3>
+            <p>
+            </p>
+
+            <h3>What data are you collecting?</h3>
+            <p>
+            </p>
+
+            <h3>Who owns the data that is collected and will you sell it?</h3>
+            <p>
+            </p>
+
+            <h3>Why are you using Bluetooth?</h3>
+            <p>
+            </p>
+
+            <h3>Will this app snoop on me?</h3>
+            <p>
+            </p>
+
+            <h3>How do I use this app?</h3>
+            <p>
+            </p>
+
+            <h3>Will this app tell me where or when I was exposed?</h3>
+            <p>
+            </p>
+
+            <h3>Who are your app’s collaborators? Are you working with the government?</h3>
+            <p>
+            </p>
+
+
+            <h3>Question?</h3>
+            <p>
+            Answer!
+            </p>
+
+
+          </div>
+        </section>
+      </v-col>
+    </v-row></v-container
+  >
+</template>

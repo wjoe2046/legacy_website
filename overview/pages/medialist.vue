848ca6823ffc04ca2ea50cbd0ffa7ebb2d73a71e
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-04-03 14:04:15 -0400

    add financial times article

--- overview/pages/medialist.vue
+++ overview/pages/medialist.vue
@@ -6,51 +6,65 @@
 
         <h1 style="color:#bf3f4a">Media Coverage</h1>
         <br>
-        <p>
-          As our project has grown, we've begun to gain some media coverage, listed here:
-        </p>
         
-        <p>
+        <div>
+          <div>April 3, 2020</div>
+          <i><a target="_blank" href="https://drive.google.com/file/d/1VH9uwJJliheVy5unCXtAHQbEdsuJdYD7/view?usp=sharing">
+            US and Europe race to develop ‘contact tracing’ apps
+          </a></i> by Financial Times.
+          <i><a target="_blank" href="https://www.ft.com/content/d42acff2-b0b5-400b-b38f-ec621d4efd95">
+            Link to paywalled article.
+          </a></i>
+        </div>
+         <br>
+
+        <div>
           <div>April 2, 2020</div>
-          <i><a href="https://venturebeat.com/2020/04/02/surveillance-ai-and-saving-lives-top-agenda-at-coronavirus-conference/">
+          <i><a target="_blank" href="https://venturebeat.com/2020/04/02/surveillance-ai-and-saving-lives-top-agenda-at-coronavirus-conference/">
             Surveillance, AI, and saving lives top agenda at coronavirus conference
           </a></i> by VentureBeat.
-        </p>
+        </div>
+         <br>
 
-        <p>
+        <div>
           <div>March 31, 2020</div>
-          <i><a href="https://www.krellinst.org/csgf/about-doe-csgf/news-events/white-covid-watch">
+          <i><a target="_blank" href="https://www.krellinst.org/csgf/about-doe-csgf/news-events/white-covid-watch">
             Fellow Working to Devise COVID-19 Contact Tracking Tool
           </a></i> by the Department of Energy Computational Science Graduate Fellowship.
-        </p>
+        </div>
+         <br>
 
-        <p>
+        <div>
           <div>March 28, 2020</div>
-          <i><a href="https://news.yahoo.com/privacy-rights-may-become-next-victim-killer-pandemic-014627271.html">
+          <i><a target="_blank" href="https://news.yahoo.com/privacy-rights-may-become-next-victim-killer-pandemic-014627271.html">
             Privacy rights may become next victim of killer pandemic
-          </a></i> by <a href="https://twitter.com/rleverafp">Rob Lever</a>, AFP.
-        </p>
+          </a></i> by <a target="_blank" href="https://twitter.com/rleverafp">Rob Lever</a>, AFP.
+        </div>
+         <br>
 
-        <p>
+        <div>
           <div>March 13, 2020</div>
-          <i><a href="https://youtu.be/7N0jC6UGzGY?t=8129">
+          <i><a target="_blank" href="https://youtu.be/7N0jC6UGzGY?t=8129">
             Radio interview
-          </a></i> with the <a href="https://twitter.com/FaultLinesRadio">Fault Lines</a> team, Radio Sputnik.
-        </p>
+          </a></i> with the <a target="_blank" href="https://twitter.com/FaultLinesRadio">Fault Lines</a> team, Radio Sputnik.
+        </div>
+         <br>
+          <br>
 
-        <p>
+        <div>
           For all media-related inquiries, please contact us at
           <b
-          ><a href="mailto: contact@covid-watch.org"
+          ><a target="_blank" href="mailto: contact@covid-watch.org"
           >contact@covid-watch.org</a
           ></b
           >.
-        </p>
+        </div>
+         <br>
 
-        <p>
+        <div>
           Lastly, if you just want to stay in the loop, you can sign up to our
           newsletter for updates:
-        </p>
+        </div>
 
         <SubscribeForm></SubscribeForm>
 

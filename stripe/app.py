cf7f87541268824850106f7475bea709c85e4db8
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-04-03 18:49:32 -0700

    unlimited cors

--- stripe/app.py
+++ stripe/app.py
@@ -21,8 +21,8 @@ def checkout():
         cancel_url='https://covid-watch.org/donate',
     )
     response = jsonify({'session_id': session.id})
-    response.headers.add('Access-Control-Allow-Origin',
-                         os.getenv('CORS_ORIGIN_URL'))
+    # response.headers.add('Access-Control-Allow-Origin', os.getenv('CORS_ORIGIN_URL'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 
